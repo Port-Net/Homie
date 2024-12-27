40613d852ce234bd0f2bd240bb733e13527165f4
Christian <git@port-net.de>
2024-12-27 17:36:14 +0100

    first commit

---
+++ src/homie.cpp
@@ -0,0 +1,358 @@
+
+#include <Arduino.h>
+#include "homie.h"
+//#include "raffstore.h"
+//#include <ArduinoJson.h>
+//#include <YAMLDuino.h>
+
+//AsyncMqttClient mqttClient;
+//TimerHandle_t mqttReconnectTimer;
+//TimerHandle_t mqttHeartbeatTimer;
+
+HOMIE_Property::HOMIE_Property(String name) : _name(name) {
+  _description = name;
+  _name.toLowerCase();
+  _settable = false;
+  _datatype = String("integer");
+  _format = String("");
+  _unit = "";
+  _only_direct = false;
+}
+
+HOMIE_Property& HOMIE_Property::description(String description) {
+  _description = description;
+  return *this;
+}
+
+HOMIE_Property& HOMIE_Property::settable(bool settable) {
+  _settable = settable;
+  return *this;
+}
+
+HOMIE_Property& HOMIE_Property::onlyDirect(bool direct) {
+  _only_direct = direct;
+  return *this;
+}
+
+HOMIE_Property& HOMIE_Property::datatype(String datatype) {
+  _datatype = datatype;
+  return *this;
+}
+
+HOMIE_Property& HOMIE_Property::format(String format) {
+  _format = format;
+  return *this;
+}
+
+HOMIE_Property& HOMIE_Property::setSetCallback(void (*callback)(HOMIE_Property* prop, String msg)) {
+  _set_callback = callback;
+  return *this;
+}
+
+HOMIE_Property& HOMIE_Property::setGetCallback(String (*callback)(HOMIE_Property* prop)) {
+  _get_callback = callback;
+  return *this;
+}
+
+String HOMIE_Property::getName() {
+  return _name;
+}
+
+String HOMIE_Property::getDatatype() {
+  return _datatype;
+}
+
+String HOMIE_Property::getFormat() {
+  return _format;
+}
+
+bool HOMIE_Property::settable() {
+  return _settable;
+}
+
+void HOMIE_Property::processSet(String topic, String msg) {
+  if(topic != _name + String("/set")) {
+    return;
+  }
+  if(_settable && _set_callback) {
+    _set_callback(this, msg);
+  }
+}
+
+void HOMIE_Property::sendValue(String topic, AsyncMqttClient* mqttClient, bool direct) {
+  if(_only_direct && !direct) {
+    return;
+  }
+  String my_topic = topic + String("/") + _name;
+  if(_get_callback) {
+    String msg = _get_callback(this);
+    mqttClient->publish(my_topic.c_str(), 1, false, msg.c_str());
+  }
+}
+
+void HOMIE_Property::sendConfig(String topic, AsyncMqttClient* mqttClient) {
+  String my_topic = topic + String("/") + _name;
+  //mqttClient->publish(new_topic.c_str(), 1, false, msg.c_str());
+  mqttClient->publish( (my_topic + String("/$name")).c_str(), 1, true, _description.c_str());
+  mqttClient->publish( (my_topic + String("/$settable")).c_str(), 1, true, _settable ? "true" : "false");
+  mqttClient->publish( (my_topic + String("/$datatype")).c_str(), 1, true, _datatype.c_str());
+  if(_format != "") {
+    mqttClient->publish( (my_topic + String("/$format")).c_str(), 1, true, _format.c_str());
+  }
+  if(_unit != "") {
+    mqttClient->publish( (my_topic + String("/$unit")).c_str(), 1, true, _unit.c_str());
+  }
+  if(_settable) {
+    mqttClient->subscribe((my_topic + String("/set")).c_str(), 1);
+  }
+
+}
+
+//////////////////////////////////////
+
+HOMIE_Node::HOMIE_Node(String name) : _name(name) {
+  _description = name;
+  _name.toLowerCase();
+  _type = "";
+}
+
+HOMIE_Node& HOMIE_Node::description(String name) {
+  _description = name;
+  return *this;
+}
+
+HOMIE_Node& HOMIE_Node::type(String type) {
+  _type = type;
+  return *this;
+}
+
+void HOMIE_Node::addProperty(HOMIE_Property* prop) {
+  _properties.push_back(prop);
+}
+
+String HOMIE_Node::getName() {
+  return(_name);
+}
+
+void HOMIE_Node::processSet(String topic, String msg) {
+  int idx = topic.indexOf("/");
+  if(idx <= 0) {
+    return;
+  }
+  String node = topic.substring(0, idx);
+  if(node == _name) {
+    String new_topic = topic.substring(idx + 1);
+    for(auto it : _properties) {
+      it->processSet(new_topic, msg);
+    }
+  }
+}
+
+void HOMIE_Node::sendValue(String topic, AsyncMqttClient* mqttClient, HOMIE_Property* ident) {
+  String new_topic = topic + String("/") + _name;
+  for(auto it : _properties) {
+    if(!ident) {
+      it->sendValue(new_topic, mqttClient);
+    } else if(ident == it) {
+      it->sendValue(new_topic, mqttClient, true);
+    }
+  }
+}
+
+void HOMIE_Node::sendConfig(String topic, AsyncMqttClient* mqttClient) {
+  String new_topic = topic + String("/") + _name;
+  mqttClient->publish( (new_topic + String("/$name")).c_str(), 1, true, _description.c_str());
+  mqttClient->publish( (new_topic + String("/$type")).c_str(), 1, true, _type.c_str());
+  String props = String("");
+  for(auto it : _properties) {
+    if(!props.isEmpty()) {
+      props += String(",");
+    }
+    props += it->getName();
+  }
+  mqttClient->publish( (new_topic + String("/$properties")).c_str(), 1, true, props.c_str());
+  for(auto it : _properties) {
+    it->sendConfig(new_topic, mqttClient);
+  }
+}
+
+////////////////////////////////////////
+
+HOMIE_Device::HOMIE_Device(String mqtt_server, uint16_t mqtt_port) : _mqtt_port(mqtt_port) {
+  getThis(this);
+  strncpy(_mqtt_server, mqtt_server.c_str(), sizeof(_mqtt_server)); // we need permanent allocated char*
+  _mqttClient = nullptr;
+  _heartbeat_interval_ms = MQTT_HEARTBEAT;
+}
+
+HOMIE_Device& HOMIE_Device::addNode(HOMIE_Node* node) {
+  _nodes.push_back(node);
+  return *this;
+}
+
+HOMIE_Device& HOMIE_Device::setFirmware(String fw) {
+  _firmware = fw;
+  return *this;
+}
+
+HOMIE_Device& HOMIE_Device::setVersion(String ver) {
+  _version = ver;
+  return *this;
+}
+
+// create a static object pointer
+HOMIE_Device* HOMIE_Device::getThis(HOMIE_Device* me) {
+  static HOMIE_Device* p_me;
+  if(me) {
+    p_me = me;
+  }
+  return p_me;
+}
+
+void HOMIE_Device::s_onConnect(bool sessionPresent) {
+  getThis()->onConnect(sessionPresent);
+}
+
+void HOMIE_Device::onConnect(bool sessionPresent) {
+  Serial.println("Connected to MQTT.");
+  Serial.print("Session present: ");
+  Serial.println(sessionPresent);
+  publish(_fullbase + String("/$state"), "init", true);
+  publish(_fullbase + String("/$homie"), "3.0.0", true);
+  publish(_fullbase + String("/$name"), _name, true);
+  publish(_fullbase + String("/$localip"), WiFi.localIP().toString(), true);
+  publish(_fullbase + String("/$mac"), WiFi.macAddress(), true);
+  publish(_fullbase + String("/$fw/name"), _firmware, true);
+  publish(_fullbase + String("/$fw/version"), _version, true);
+  publish(_fullbase + String("/$implementation"), "arduino esp32", true);
+  publish(_fullbase + String("/$stats"), "uptime", true);
+  publish(_fullbase + String("/$stats/uptime"), millis()/1000, true);
+  publish(_fullbase + String("/$stats/interval"), _heartbeat_interval_ms/1000, true);
+  String nodes = String("");
+  for(auto it : _nodes) {
+    if(!nodes.isEmpty()) {
+      nodes += String(",");
+    }
+    nodes += it->getName();
+  }
+  publish(_fullbase + String("/$nodes"), nodes, true);
+  for(auto it : _nodes) {
+    it->sendConfig(_fullbase, _mqttClient);
+  }
+  publish(_fullbase + String("/$state"), "ready", true);
+  for(auto it : _nodes) {
+    it->sendValue(_fullbase, _mqttClient);
+  }
+  xTimerStart(_mqttHeartbeatTimer, 0);
+}
+
+void HOMIE_Device::s_onDisconnect(AsyncMqttClientDisconnectReason reason) {
+  xTimerStop(getThis()->_mqttHeartbeatTimer, 0);
+}
+
+void HOMIE_Device::s_onMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
+  getThis()->onMessage(topic, payload, properties, len, index, total);
+}
+
+void HOMIE_Device::onMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total) {
+  if(!len) {
+    return;
+  }
+  String topicstring(topic);
+  char pl[len+1];
+  pl[len] = '\0';
+  strncpy(pl, payload, len);
+  String payloadstring(pl);
+  if(!topicstring.startsWith(_fullbase + String("/"))) {
+    return;
+  }
+  topicstring = topicstring.substring(_fullbase.length() + 1);
+  for(auto it : _nodes) {
+    it->processSet(topicstring, payloadstring);
+  }
+}
+
+void HOMIE_Device::s_Heartbeat() {
+  getThis()->Heartbeat();
+}
+
+void HOMIE_Device::Heartbeat() {
+  if (_mqttClient->connected()) {
+    publish( _fullbase + String("/$stats/uptime"), millis()/1000, true);
+    // TODO handle additional stats
+  } else {
+    if (WiFi.isConnected()) {
+      _mqttClient->connect();
+    }
+  }
+}
+
+void HOMIE_Device::setHeartbeatInterval(uint32_t hb_time_ms) {
+  _heartbeat_interval_ms = hb_time_ms;
+}
+
+
+void HOMIE_Device::begin(String name, String base) {
+  _name = name;
+  _base = base;
+  _fullbase = base + String("/") + name;
+  _mqttClient = new AsyncMqttClient();
+  static char server[50];
+  _mqttClient->setServer(_mqtt_server, _mqtt_port);
+  strncpy(_will_topic, (_fullbase + String("/$state")).c_str(), sizeof(_will_topic)); // we need permanent allocated char*
+  _mqttClient->setWill(_will_topic, 1, true, "lost");
+  _mqttClient->onConnect(s_onConnect);
+  _mqttClient->onDisconnect(s_onDisconnect);
+  _mqttClient->onMessage(s_onMessage);
+  _mqttReconnectTimer = xTimerCreate("mqttReconnTimer", pdMS_TO_TICKS(MQTT_RECONNECT), pdTRUE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(s_connect));
+  _mqttHeartbeatTimer = xTimerCreate("mqttHBTimer", pdMS_TO_TICKS(_heartbeat_interval_ms), pdTRUE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(s_Heartbeat));
+  if(WiFi.isConnected()) {
+    _mqttClient->connect();
+  }
+  xTimerStart(_mqttReconnectTimer, 0);
+}
+
+void HOMIE_Device::s_connect() {
+  getThis()->connect();
+}
+
+void HOMIE_Device::connect() {
+  if(_mqttClient) {
+    if(_mqttClient->connected()) {
+      return;
+    }
+    if(WiFi.isConnected()) {
+      _mqttClient->connect();
+    }
+  }
+}
+
+void HOMIE_Device::publish(String topic, const char* msg, bool retain) {
+  if (!_mqttClient->connected()) {
+    return;
+  }
+  _mqttClient->publish( topic.c_str(), 1, retain, msg);
+}
+
+void HOMIE_Device::publish(String topic, String msg, bool retain) {
+  if (!_mqttClient->connected()) {
+    return;
+  }
+  _mqttClient->publish( topic.c_str(), 1, retain, msg.c_str());
+}
+
+void HOMIE_Device::publish(String topic, int data, bool retain) {
+  if (!_mqttClient->connected()) {
+    return;
+  }
+  _mqttClient->publish( topic.c_str(), 1, retain, String(data).c_str());
+}
+
+void HOMIE_Device::sendUpdates(HOMIE_Property* ident) {
+  if(!_mqttClient->connected()) {
+    return;
+  }
+  for(auto it : _nodes) {
+    it->sendValue(_fullbase, _mqttClient, ident);
+  }
+}
---
+++ src/homie.h
@@ -0,0 +1,104 @@
+#ifndef MQTT_H
+#define MQTT_H
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <AsyncMqttClient.h>
+#include <vector>
+
+#define MQTT_RECONNECT 10000L
+#define MQTT_HEARTBEAT 60000L
+
+class HOMIE_Property {
+public:
+  HOMIE_Property(String name);
+  HOMIE_Property& description(String description);
+  HOMIE_Property& settable(bool settable);
+  HOMIE_Property& onlyDirect(bool direct);
+  HOMIE_Property& datatype(String datatype);
+  HOMIE_Property& format(String format);
+  HOMIE_Property& setSetCallback(void (*callback)(HOMIE_Property* prop, String msg));
+  HOMIE_Property& setGetCallback(String (*callback)(HOMIE_Property* prop));
+  String getName();
+  String getDatatype();
+  String getFormat();
+  bool settable();
+  void processSet(String topic, String msg);
+  void sendValue(String topic, AsyncMqttClient* _mqttClient, bool direct = false);
+  void sendConfig(String topic, AsyncMqttClient* _mqttClient);
+private:
+  String _name;  
+  String _description;  
+  bool _settable;
+  String _datatype;  
+  String _format;
+  String _unit;
+  bool _only_direct;
+
+  void (*_set_callback)(HOMIE_Property* prop, String msg);
+  String (*_get_callback)(HOMIE_Property* prop);
+};
+
+class HOMIE_Node {
+public:
+  HOMIE_Node(String name);
+  HOMIE_Node& description(String description);
+  HOMIE_Node& type(String type);
+  void addProperty(HOMIE_Property* prop);
+  String getName();
+  void processSet(String topic, String msg);
+  void sendValue(String topic, AsyncMqttClient* _mqttClient, HOMIE_Property* ident = nullptr);
+  void sendConfig(String topic, AsyncMqttClient* _mqttClient);
+private:
+  String _name;
+  String _description;
+  String _type;
+  std::vector<HOMIE_Property*> _properties;
+};
+
+class HOMIE_Device {
+public:
+  HOMIE_Device(String mqtt_server, uint16_t mqtt_port = 1883);
+  HOMIE_Device& addNode(HOMIE_Node* node);
+  HOMIE_Device& setFirmware(String fw);
+  HOMIE_Device& setVersion(String ver);
+  void begin(String name, String base = "homie");
+  void setHeartbeatInterval(uint32_t hb_time_ms);
+  void sendUpdates(HOMIE_Property* ident = nullptr);
+  void connect();
+//protected:
+//  void publish_int(const char* subtopic, int data);
+//  void publish_float(const char* subtopic, float data);
+//  void addCallback(void (*func)(String topic, String msg));
+private:
+  static HOMIE_Device* getThis( HOMIE_Device* me = nullptr);
+  static void s_connect();
+  static void s_onConnect(bool sessionPresent);
+  static void s_onDisconnect(AsyncMqttClientDisconnectReason reason);
+  static void s_onMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total);
+  static void s_Heartbeat();
+  void onConnect(bool sessionPresent);
+  //void onDisconnect(AsyncMqttClientDisconnectReason reason);
+  void onMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total);
+  void Heartbeat();
+  void publish(String topic, const char* msg, bool retain = false);
+  void publish(String topic, String msg, bool retain = false);
+  void publish(String topic, int data, bool retain = false);
+  //static MY_MQTT* __our_obj;
+  char _mqtt_server[50];
+  uint16_t _mqtt_port;
+  String _name;
+  String _firmware;
+  String _version;
+  String _base;
+  String _fullbase;
+  char _will_topic[50];
+  uint32_t _heartbeat_interval_ms;
+  AsyncMqttClient* _mqttClient;
+  TimerHandle_t _mqttReconnectTimer;
+  TimerHandle_t _mqttHeartbeatTimer;
+  void (*_callback)(String topic, String msg);
+  std::vector<HOMIE_Node*> _nodes;
+};
+
+#endif
